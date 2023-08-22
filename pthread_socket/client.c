d84b276576a40b89960e6efb5100253070692d10
hpsprger <281872614@qq.com>
2023-08-23 01:23:28 +0800

    mdf

--- pthread_socket/client.c
+++ pthread_socket/client.c
@@ -1,102 +1,134 @@
 /*
 * 客户端代码
 */
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
 #include "config.h"
 
-/*处理接收服务器消息函数*/
-void *recv_message(void *fd)
-{
-	int sockfd = *(int *)fd;
-	while(1)
-	{
-		char buf[MAX_LINE];
-		memset(buf , 0 , MAX_LINE);
-		int n;
-		if((n = recv(sockfd , buf , MAX_LINE , 0)) == -1)
-		{
-			perror("recv error.\n");
-			exit(1);
-		}//if
-		buf[n] = '\0';
-		
-		//若收到的是exit字符，则代表退出通信
-		if(strcmp(buf , "byebye.") == 0)
-		{
-			printf("Server is closed.\n");
-			close(sockfd);
-			exit(0);
-		}//if
 
-		printf("\nServer: %s\n", buf);
-	}//while
+int sockfd;
+
+#define DATA_MAX_LEN 4096 
+#define MAX_LINE 1024
+
+typedef struct _msg {
+	unsigned short type;
+	unsigned short len;
+	unsigned char data[DATA_MAX_LEN];
+} msg;
+
+/*处理接收客户端消息函数*/
+void *send_message(void)
+{
+	msg tx_msg;
+	memset(&tx_msg , 0 , sizeof(tx_msg));
+	tx_msg.type = 0x1234;
+	printf("input msg:");
+	if (fgets(&tx_msg.data , DATA_MAX_LEN , stdin) != NULL) {
+		tx_msg.len = strlen(tx_msg.data) + 4;
+		if(send(sockfd, &tx_msg, tx_msg.len, 0) == -1)
+		{
+			perror("send error.\n");
+			return;
+		}
+	}
 }
 
+void *recv_message(void)
+{
+	msg rcv_msg;
+	memset(&rcv_msg, 0, sizeof(rcv_msg));
+	int n;
+
+	if((n = recv(sockfd, &rcv_msg, DATA_MAX_LEN, 0)) == -1)
+	{
+		perror("recv error.\n");
+		return;
+	}
+	printf("\nrec: n=%d\n", n);
+	printf("recv: rcv_msg.type=%d\n", rcv_msg.type);
+	printf("recv: rcv_msg.len=%d\n",  rcv_msg.len);
+	printf("recv: rcv_msg.data=%s\n", rcv_msg.data);
+}
+
+void *client_entry()
+{
+    struct sockaddr_in servaddr;
+	struct tcp_info info; 
+	int len=sizeof(info);
+
+	while (1) {
+		if((sockfd = socket(AF_INET , SOCK_STREAM , 0)) == -1)
+		{
+			perror("socket error");
+			exit(1);
+		}
+
+		bzero(&servaddr , sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_port = htons(PORT);
+		if(inet_pton(AF_INET , "127.0.0.1" , &servaddr.sin_addr) < 0)
+		{
+			printf("inet_pton error\n");
+			close(sockfd);
+			continue;
+		}
+
+		if( connect(sockfd , (struct sockaddr *)&servaddr , sizeof(servaddr)) < 0)
+		{
+			perror("connect error");
+			close(sockfd);
+			continue;
+		}
+
+		while (1) {
+			getsockopt(sockfd, IPPROTO_TCP, TCP_INFO, &info, (socklen_t *)&len);
+			if (info.tcpi_state == TCP_ESTABLISHED) {
+				sleep(1);
+				continue;
+			} else {
+				break;
+			}
+		}
+	}
+}
 
 int main(int argc , char **argv)
 {
-	/*声明套接字和链接服务器地址*/
-    int sockfd;
-	pthread_t recv_tid , send_tid;
-    struct sockaddr_in servaddr;
+	char msg[MAX_LINE];
+	pthread_t tid;
 
-    /*判断是否为合法输入*/
-    if(argc != 2)
-    {
-        perror("usage:tcpcli <IPaddress>");
-        exit(1);
-    }//if
-
-    /*(1) 创建套接字*/
-    if((sockfd = socket(AF_INET , SOCK_STREAM , 0)) == -1)
-    {
-        perror("socket error");
-        exit(1);
-    }//if
-
-    /*(2) 设置链接服务器地址结构*/
-    bzero(&servaddr , sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    if(inet_pton(AF_INET , argv[1] , &servaddr.sin_addr) < 0)
-    {
-        printf("inet_pton error for %s\n",argv[1]);
-        exit(1);
-    }//if
-
-    /*(3) 发送链接服务器请求*/
-    if( connect(sockfd , (struct sockaddr *)&servaddr , sizeof(servaddr)) < 0)
-    {
-        perror("connect error");
-        exit(1);
-    }//if	
-
-	/*创建子线程处理该客户链接接收消息*/
-	if(pthread_create(&recv_tid , NULL , recv_message, &sockfd) == -1)
+	if(pthread_create(&tid , NULL , client_entry, 0) == -1)
 	{
 		perror("pthread create error.\n");
 		exit(1);
-	}//if	
+	}
 
-	/*处理客户端发送消息*/
-	char msg[MAX_LINE];
-	memset(msg , 0 , MAX_LINE);
-	while(fgets(msg , MAX_LINE , stdin) != NULL)	
-	{
-		if(strcmp(msg , "exit\n") == 0)
-		{
-			printf("byebye.\n");
-			memset(msg , 0 , MAX_LINE);
-			strcpy(msg , "byebye.");
-			send(sockfd , msg , strlen(msg) , 0);
-			close(sockfd);
-			exit(0);
-		}//if
-		if(send(sockfd , msg , strlen(msg) , 0) == -1)
-		{
-			perror("send error.\n");
-			exit(1);
-		}//if
-	
-		
-	}//while
+	while (1) {
+		printf("pls choose: 1.send 2.recv 3.exit \n");
+		memset(msg , 0 , MAX_LINE);
+		if ((fgets(msg , MAX_LINE , stdin)) != NULL) {
+			if(strcmp(msg , "1\n") == 0)
+			{
+				send_message();
+			}
+			if(strcmp(msg , "2\n") == 0)
+			{
+				recv_message();
+			}
+			if(strcmp(msg , "3\n") == 0)
+			{
+				printf("Client closed.\n");
+				close(sockfd);
+				exit(1);
+			}
+		}
+	}
 }
--- pthread_socket/server.c
+++ pthread_socket/server.c
@@ -2,138 +2,131 @@
 *  服务器端代码实现
 */
 
+
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
 #include "config.h"
 
 //声明套接字
 int listenfd, connfd;
 
+#define DATA_MAX_LEN 4096 
+#define MAX_LINE 1024
+
+typedef struct _msg {
+	unsigned short type;
+	unsigned short len;
+	unsigned char data[DATA_MAX_LEN];
+} msg;
+
 /*处理接收客户端消息函数*/
 void *send_message(void)
 {
-	char msg[MAX_LINE];
-	memset(msg , 0 , MAX_LINE);
-	if (fgets(msg , MAX_LINE , stdin) != NULL) {
-		if(strcmp(msg , "exit\n") == 0)
-		{
-			printf("byebye.\n");
-			memset(msg , 0 , MAX_LINE);
-			strcpy(msg , "byebye.");
-			send(connfd , msg , strlen(msg) , 0);
-			close(connfd);
-			exit(0);
-		}
-		if(send(connfd , msg , strlen(msg) , 0) == -1)
+	msg tx_msg;
+	memset(&tx_msg , 0 , sizeof(tx_msg));
+	tx_msg.type = 0x1234;
+	printf("input msg:");
+	if (fgets(tx_msg.data, DATA_MAX_LEN, stdin) != NULL) {
+		tx_msg.len = strlen(tx_msg.data) + 4;
+		if(send(connfd, &tx_msg, tx_msg.len, 0) == -1)
 		{
 			perror("send error.\n");
-			exit(1);
+			return;
 		}
 	}
 }
 
-void *recv_message(void *fd)
+void *recv_message(void)
 {
-	int sockfd = *(int *)fd;
-	while(1)
-	{
-		char buf[MAX_LINE];
-		memset(buf , 0 , MAX_LINE);
-		int n;
-		if((n = recv(sockfd , buf , MAX_LINE , 0)) == -1)
-		{
-			perror("recv error.\n");
-			exit(1);
-		}//if
-		buf[n] = '\0';		
-		//若收到的是exit字符，则代表退出通信
-		if(strcmp(buf , "byebye.") == 0)
-		{
-			printf("Client closed.\n");
-			close(sockfd);
-			exit(1);
-		}//if
+	msg rcv_msg;
+	memset(&rcv_msg, 0, sizeof(rcv_msg));
+	int n;
 
-		printf("\nClient: %s\n", buf);
-	}//while
+	if((n = recv(connfd, &rcv_msg, DATA_MAX_LEN, 0)) == -1)
+	{
+		perror("recv error.\n");
+		return;
+	}
+	printf("\nrec: n=%d\n", n);
+	printf("recv: rcv_msg.type=%d\n", rcv_msg.type);
+	printf("recv: rcv_msg.len=%d\n",  rcv_msg.len);
+	printf("recv: rcv_msg.data=%s\n", rcv_msg.data);
 }
 
 void *server_entry()
 {
 	socklen_t clilen;
-	//声明线程ID
 	pthread_t recv_tid;
 
-	//定义地址结构
 	struct sockaddr_in servaddr , cliaddr;
 	
-	/*(1) 创建套接字*/
 	if((listenfd = socket(AF_INET , SOCK_STREAM , 0)) == -1)
 	{
 		perror("socket error.\n");
 		exit(1);
-	}//if
+	}
 
-	/*(2) 初始化地址结构*/
 	bzero(&servaddr , sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(PORT);
 
-	/*(3) 绑定套接字和端口*/
 	if(bind(listenfd , (struct sockaddr *)&servaddr , sizeof(servaddr)) < 0)
 	{
 		perror("bind error.\n");
 		exit(1);
-	}//if
+	}
 
-	/*(4) 监听*/
 	if(listen(listenfd , LISTENQ) < 0)
 	{
 		perror("listen error.\n");
 		exit(1);
-	}//if
+	}
 
-	/*(5) 接受客户请求，并创建线程处理*/
 	while (1) {
 		clilen = sizeof(cliaddr);
 		if((connfd = accept(listenfd , (struct sockaddr *)&cliaddr , &clilen)) < 0)
 		{
 			perror("accept error.\n");
 			exit(1);
-		}//if
+		}
 		printf("server: got connection from %s\n", inet_ntoa(cliaddr.sin_addr));
-		/*创建子线程处理该客户链接接收消息*/
-		if(pthread_create(&recv_tid , NULL , recv_message, &connfd) == -1)
-		{
-			perror("pthread create error.\n");
-			exit(1);
-		}//if
 	}
 }
 
 int main()
 {
 	char msg[MAX_LINE];
-	pthread_t recv_tid;
+	pthread_t tid;
 
 	printf("socket server main...\n");
 
-	/*创建子线程处理该客户链接接收消息*/
-	if(pthread_create(&recv_tid , NULL , server_entry, 0) == -1)
+	if(pthread_create(&tid , NULL , server_entry, 0) == -1)
 	{
 		perror("pthread create error.\n");
 		exit(1);
-	}//if
+	}
 
 	while (1) {
-		printf("pls choose:");
+		printf("pls choose: 1.send 2.recv 3.exit \n");
 		memset(msg , 0 , MAX_LINE);
 		if ((fgets(msg , MAX_LINE , stdin)) != NULL) {
-			if(strcmp(msg , "send\n") == 0)
+			if(strcmp(msg , "1\n") == 0)
 			{
-				printf("pls input send msg:");
 				send_message();
 			}
-			if(strcmp(msg , "exit\n") == 0)
+			if(strcmp(msg , "2\n") == 0)
+			{
+				recv_message();
+			}
+			if(strcmp(msg , "3\n") == 0)
 			{
 				printf("Client closed.\n");
 				close(listenfd);
