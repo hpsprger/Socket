1814fb0da0bd06c666cfdacf7417bd1dd422350c
hpsprger <281872614@qq.com>
2023-08-24 01:49:00 +0800

    mdf

--- pthread_socket/client.c
+++ pthread_socket/client.c
@@ -16,48 +16,87 @@
 int sockfd;
 
 #define DATA_MAX_LEN 4096 
-#define MAX_LINE 1024
+#define CHOOSE_MAX_LINE 50
 
-typedef struct _msg {
+typedef struct _super_msg {
 	unsigned short type;
 	unsigned short len;
-	unsigned char data[DATA_MAX_LEN];
-} msg;
+	unsigned char  *buffer;
+} super_msg;
 
 /*处理接收客户端消息函数*/
-void *send_message(void)
+int send_message(super_msg *pmsg)
 {
-	msg tx_msg;
-	memset(&tx_msg , 0 , sizeof(tx_msg));
-	tx_msg.type = 0x1234;
-	printf("input msg:");
-	if (fgets(&tx_msg.data , DATA_MAX_LEN , stdin) != NULL) {
-		tx_msg.len = strlen(tx_msg.data) + 4;
-		if(send(sockfd, &tx_msg, tx_msg.len, 0) == -1)
-		{
-			perror("send error.\n");
-			return;
-		} else {
-			printf("send %d bytes ok!\n", tx_msg.len);
-		}
+	struct msghdr tx_msg = {0};
+	struct iovec io = {0};
+	char msg_ctrl_buf[CMSG_SPACE(sizeof(unsigned short) + sizeof(unsigned short))];
+	struct cmsghdr *cmsg;
+
+	if (pmsg == NULL) {
+		return -1;
+	} 
+
+	io.iov_base = pmsg->buffer;
+	io.iov_len = pmsg->len;
+
+	tx_msg.msg_iov = &io;
+	tx_msg.msg_iovlen = 2;
+	tx_msg.msg_control = msg_ctrl_buf;
+	tx_msg.msg_controllen = sizeof(msg_ctrl_buf);
+
+	cmsg = CMSG_FIRSTHDR(&tx_msg);
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_RIGHTS;	
+	cmsg->cmsg_len = CMSG_LEN(sizeof(unsigned short) + sizeof(unsigned short));
+
+	((super_msg *)CMSG_DATA(cmsg))->type = pmsg->type;
+	((super_msg *)CMSG_DATA(cmsg))->len = pmsg->len;
+
+	if(sendmsg(sockfd, &tx_msg, 0) < 0) {
+		perror("send error.\n");
+		return -1;
 	}
+	return 0;
 }
 
-void *recv_message(void)
+/* 返回接收的总长度 */
+int recv_message(super_msg *pmsg, unsigned int rx_len_max, unsigned int timeout)
 {
-	msg rcv_msg;
-	memset(&rcv_msg, 0, sizeof(rcv_msg));
-	int n;
+	int ret;
+	int flag;
+	struct msghdr rx_msg = {0};
+	struct iovec io = {0};
+	char msg_ctrl_buf[CMSG_SPACE(sizeof(unsigned short) + sizeof(unsigned short))];
+	struct cmsghdr *cmsg;
+	struct timeval time;
 
-	if((n = recv(sockfd, &rcv_msg, DATA_MAX_LEN, 0)) == -1)
-	{
-		perror("recv error.\n");
-		return;
+	io.iov_base = pmsg->buffer;
+	io.iov_len = rx_len_max;
+
+	rx_msg.msg_control = msg_ctrl_buf;
+	rx_msg.msg_controllen = sizeof(msg_ctrl_buf);
+
+	if (timeout == 0) {
+		flag = MSG_DONTWAIT;
+	} else {
+		flag = MSG_WAITALL;
+		time.tv_sec = 0;
+		time.tv_usec = timeout;
+		if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&time, sizeof(time)) == -1) {
+			printf("recv_message setsockopt:%08x  SO_RCVTIMEO error\n", sockfd);						
+		}
 	}
-	printf("\nrec: n=%d\n", n);
-	printf("recv: rcv_msg.type=%d\n", rcv_msg.type);
-	printf("recv: rcv_msg.len=%d\n",  rcv_msg.len);
-	printf("recv: rcv_msg.data=%s\n", rcv_msg.data);
+
+	ret = recvmsg(sockfd, &rx_msg, flag);
+	if(ret < 0) {
+		perror("send error.\n");
+		return -1;
+	}
+
+	cmsg = CMSG_FIRSTHDR(&rx_msg);
+	pmsg->type = ((super_msg *)CMSG_DATA(cmsg))->type;
+	pmsg->len = ((super_msg *)CMSG_DATA(cmsg))->len;
+	return ret;
 }
 
 void *client_entry()
@@ -116,10 +155,16 @@ void *client_entry()
 	}
 }
 
+char send_text[DATA_MAX_LEN];
+char rcv_text[DATA_MAX_LEN];
+
 int main(int argc , char **argv)
 {
-	char msg[MAX_LINE];
+	char choose[CHOOSE_MAX_LINE];
 	pthread_t tid;
+	super_msg tx_msg = {0};
+	super_msg rx_msg = {0};	
+	int ret;
 
 	if(pthread_create(&tid , NULL , client_entry, 0) == -1)
 	{
@@ -129,17 +174,31 @@ int main(int argc , char **argv)
 
 	while (1) {
 		printf("pls choose: 1.send 2.recv 3.exit \n");
-		memset(msg , 0 , MAX_LINE);
-		if ((fgets(msg , MAX_LINE , stdin)) != NULL) {
-			if(strcmp(msg , "1\n") == 0)
+		memset(choose , 0 , CHOOSE_MAX_LINE);
+		if ((fgets(choose , CHOOSE_MAX_LINE , stdin)) != NULL) {
+			if(strcmp(choose , "1\n") == 0)
 			{
-				send_message();
+				printf("input msg: \n");
+				memset(send_text , 0 , DATA_MAX_LEN);
+				fgets(send_text , DATA_MAX_LEN , stdin);
+				tx_msg.type = 0x1234;
+				tx_msg.len = strlen(send_text);
+				tx_msg.buffer = send_text;
+				if (send_message(&tx_msg) < 0) {
+					printf("send_message fail \n");
+				} else {
+					printf("send_message ok \n");
+				}
 			}
-			if(strcmp(msg , "2\n") == 0)
+			if(strcmp(choose , "2\n") == 0)
 			{
-				recv_message();
+				ret = recv_message(&rx_msg, DATA_MAX_LEN, 1000);
+				printf("recv total len:%d \n", ret);
+				printf("rx msg:type(0x%x) \n", rx_msg.type);
+				printf("rx msg:len(%d) \n", rx_msg.len);
+				printf("rx msg:buffer(%s) \n", rx_msg.buffer);
 			}
-			if(strcmp(msg , "3\n") == 0)
+			if(strcmp(choose , "3\n") == 0)
 			{
 				printf("Client closed.\n");
 				close(sockfd);
--- pthread_socket/server.c
+++ pthread_socket/server.c
@@ -20,57 +20,87 @@ int connfd = -1;
 int connfd_new = -1;
 
 #define DATA_MAX_LEN 4096 
-#define MAX_LINE 1024
+#define CHOOSE_MAX_LINE 50
 
-typedef struct _msg {
+typedef struct _super_msg {
 	unsigned short type;
 	unsigned short len;
-	unsigned char data[DATA_MAX_LEN];
-} msg;
+	unsigned char  *buffer;
+} super_msg;
 
 /*处理接收客户端消息函数*/
-void *send_message(void)
+int send_message(super_msg *pmsg)
 {
-	msg tx_msg;
-	memset(&tx_msg , 0 , sizeof(tx_msg));
-	tx_msg.type = 0x1234;
-	printf("input msg:");
-	if (fgets(tx_msg.data, DATA_MAX_LEN, stdin) != NULL) {
-		tx_msg.len = strlen(tx_msg.data) + 4;
-		if(send(connfd, &tx_msg, tx_msg.len, 0) == -1)
-		{
-			perror("send error.\n");
-			return;
-		} else {
-			printf("send %d bytes ok!\n", tx_msg.len);
-		}
+	struct msghdr tx_msg = {0};
+	struct iovec io = {0};
+	char msg_ctrl_buf[CMSG_SPACE(sizeof(unsigned short) + sizeof(unsigned short))];
+	struct cmsghdr *cmsg;
+
+	if (pmsg == NULL) {
+		return -1;
+	} 
+
+	io.iov_base = pmsg->buffer;
+	io.iov_len = pmsg->len;
+
+	tx_msg.msg_iov = &io;
+	tx_msg.msg_iovlen = 2;
+	tx_msg.msg_control = msg_ctrl_buf;
+	tx_msg.msg_controllen = sizeof(msg_ctrl_buf);
+
+	cmsg = CMSG_FIRSTHDR(&tx_msg);
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_RIGHTS;	
+	cmsg->cmsg_len = CMSG_LEN(sizeof(unsigned short) + sizeof(unsigned short));
+
+	((super_msg *)CMSG_DATA(cmsg))->type = pmsg->type;
+	((super_msg *)CMSG_DATA(cmsg))->len = pmsg->len;
+
+	if(sendmsg(connfd, &tx_msg, 0) < 0) {
+		perror("send error.\n");
+		return -1;
 	}
+	return 0;
 }
 
-char *rx_buffer[DATA_MAX_LEN];
-void *recv_message(void)
+/* 返回接收的总长度 */
+int recv_message(super_msg *pmsg, unsigned int rx_len_max, unsigned int timeout)
 {
-	memset(&rx_buffer, 0, sizeof(rx_buffer));
-	int n;
-	int cur_len;
-	int idx = 0;
-	char *cur_pos; 
-	if((n = recv(connfd, rx_buffer, DATA_MAX_LEN, 0)) == -1)
-	{
-		perror("recv error.\n");
-		return;
+	int ret;
+	int flag;
+	struct msghdr rx_msg = {0};
+	struct iovec io = {0};
+	char msg_ctrl_buf[CMSG_SPACE(sizeof(unsigned short) + sizeof(unsigned short))];
+	struct cmsghdr *cmsg;
+	struct timeval time;
+
+	io.iov_base = pmsg->buffer;
+	io.iov_len = rx_len_max;
+
+	rx_msg.msg_control = msg_ctrl_buf;
+	rx_msg.msg_controllen = sizeof(msg_ctrl_buf);
+
+	if (timeout == 0) {
+		flag = MSG_DONTWAIT;
+	} else {
+		flag = MSG_WAITALL;
+		time.tv_sec = 0;
+		time.tv_usec = timeout;
+		if (setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&time, sizeof(time)) == -1) {
+			printf("recv_message setsockopt:%08x  SO_RCVTIMEO error\n", connfd);						
+		}
 	}
-	printf("\nrecv total len:%d\n", n);
-	cur_len = n;
-	cur_pos = &rx_buffer;
-	while (cur_len) {
-		printf("msg_idx:%d\n", idx++);
-		printf("recv: rcv_msg.type=%d\n", ((msg *)cur_pos)->type);
-		printf("recv: rcv_msg.len=%d\n",  ((msg *)cur_pos)->len);
-		printf("recv: rcv_msg.data=%s\n", ((msg *)cur_pos)->data);
-		cur_len -= ((msg *)cur_pos)->len;
-		cur_pos = cur_pos + ((msg *)cur_pos)->len;
+
+	ret = recvmsg(connfd, &rx_msg, flag);
+	if(ret < 0) {
+		perror("send error.\n");
+		return -1;
 	}
+
+	cmsg = CMSG_FIRSTHDR(&rx_msg);
+	pmsg->type = ((super_msg *)CMSG_DATA(cmsg))->type;
+	pmsg->len = ((super_msg *)CMSG_DATA(cmsg))->len;
+	return ret;
 }
 
 void *server_entry()
@@ -129,10 +159,16 @@ void *server_entry()
 	}
 }
 
+char send_text[DATA_MAX_LEN];
+char rcv_text[DATA_MAX_LEN];
+
 int main()
 {
-	char msg[MAX_LINE];
+	char choose[CHOOSE_MAX_LINE];
 	pthread_t tid;
+	super_msg tx_msg = {0};
+	super_msg rx_msg = {0};	
+	int ret;
 
 	printf("socket server main...\n");
 
@@ -144,19 +180,34 @@ int main()
 
 	while (1) {
 		printf("pls choose: 1.send 2.recv 3.exit \n");
-		memset(msg , 0 , MAX_LINE);
-		if ((fgets(msg , MAX_LINE , stdin)) != NULL) {
-			if(strcmp(msg , "1\n") == 0)
+		memset(choose , 0 , CHOOSE_MAX_LINE);
+		if ((fgets(choose , CHOOSE_MAX_LINE , stdin)) != NULL) {
+			if(strcmp(choose , "1\n") == 0)
 			{
-				send_message();
+				printf("input msg: \n");
+				memset(send_text , 0 , DATA_MAX_LEN);
+				fgets(send_text , DATA_MAX_LEN , stdin);
+				tx_msg.type = 0x1234;
+				tx_msg.len = strlen(send_text);
+				tx_msg.buffer = send_text;
+				if (send_message(&tx_msg) < 0) {
+					printf("send_message fail \n");
+				} else {
+					printf("send_message ok \n");
+				}
 			}
-			if(strcmp(msg , "2\n") == 0)
+			if(strcmp(choose , "2\n") == 0)
 			{
-				recv_message();
+				ret = recv_message(&rx_msg, DATA_MAX_LEN, 1000);
+				printf("recv total len:%d \n", ret);
+				printf("rx msg:type(0x%x) \n", rx_msg.type);
+				printf("rx msg:len(%d) \n", rx_msg.len);
+				printf("rx msg:buffer(%s) \n", rx_msg.buffer);
 			}
-			if(strcmp(msg , "3\n") == 0)
+			if(strcmp(choose , "3\n") == 0)
 			{
 				printf("Client closed.\n");
+				close(connfd);
 				close(listenfd);
 				exit(1);
 			}
